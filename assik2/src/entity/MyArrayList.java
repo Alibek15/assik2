f587577749b613bc092aa419a59a433fda0b23b2
Alibek15 <2018nightmare19@gmail.com>
2023-04-13 23:08:28 +0600

    implementation

--- assik2/src/entity/MyArrayList.java
+++ assik2/src/entity/MyArrayList.java
@@ -1,5 +1,7 @@
 package entity;
 
+import interfaces.MyList;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -10,4 +12,28 @@ public class MyArrayList implements MyList {
     private Object[] listElements;
     private int size;
 
+    @Override
+    public void add(final Object element) {
+
+    }
+
+    @Override
+    public void get(final int index) {
+
+    }
+
+    @Override
+    public void remove(final int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
 }
