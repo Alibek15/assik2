aa97a8f18beb43028ad70be3be727456a7bd0bad
Alibek15 <2018nightmare19@gmail.com>
2023-04-17 19:56:50 +0600

    creating the arraylist model

--- assik2/src/entity/MyArrayList.java
+++ assik2/src/entity/MyArrayList.java
@@ -2,15 +2,14 @@ package entity;
 
 import interfaces.MyList;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
-public class MyArrayList implements MyList {
+public class MyArrayList<T> implements MyList<T> {
+
 
     private Object[] listElements;
     private int size;
+
 
     public MyArrayList(final int size) {
         this.size = size;
@@ -33,23 +32,111 @@ public class MyArrayList implements MyList {
     }
 
     @Override
-    public Object get(final int index) {
-        return listElements[index];
+    public void add(final Object element, final int index) {
+        int currentLength = listElements.length;
+        if(size == currentLength){
+            int newSize = currentLength*2;
+            Object[] array = new Object[newSize];
+            for (int i = 0; i < currentLength; i++) {
+                array[i] = listElements[i];
+            }
+            listElements=array;
+        }
+        listElements[index] = element;
+    }
+
+    @Override
+    public T get(final int index) {
+        return (T) listElements[index];
     }
 
 
     @Override
-    public void remove(final int index) {
+    public T remove(final int index) {
+        int currentLength = listElements.length;
+        for (int i = index; i <size-1; i++) {
+           listElements[i]=listElements[i+1];
+        }
+        listElements[size-1]=null;
+        size--;
+        return get(index);
+    }
 
+    @Override
+    public boolean remove(final Object item) {
+        int currentLength = listElements.length;
+        for (int i = 0; i < currentLength; i++) {
+            if(item!=listElements[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        int currentLength = listElements.length;
+        for (int i = 0; i <currentLength ; i++) {
+            listElements[i] = null;
+        }
+
+    }
+
+    @Override
+    public int indexOf(final Object o) {
+        int currentLength = listElements.length;
+        for (int i = 0; i <currentLength ; i++) {
+            if(o.equals(listElements[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void sort() {
+        Arrays.sort(listElements);
+    }
+
+    @Override
+    public int lastIndexOf(final Object o) {
+
+
+        return indexOf(o);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public boolean contains(final Object o) {
+        int currentLength = listElements.length;
+        for (int i = 0; i <currentLength ; i++) {
+            if(o.equals(listElements[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<T> {
+        private int cursor;
+
+        @Override
+        public boolean hasNext() {
+            return cursor < size();
+        }
+
+        @Override
+        public T next() {
+            return get(cursor++);
+        }
     }
 }
--- assik2/src/interfaces/MyList.java
+++ assik2/src/interfaces/MyList.java
@@ -1,11 +1,21 @@
 package interfaces;
 
 public interface MyList<T> extends Iterable<T> {
+    int size();
+    boolean contains(Object o);
+
     void add(T element);
+    void add(T element, int index);
+
+
     T get(int index);
 
-    void remove(int index);
+    T remove(int index);
+    boolean remove(T item);
+    void clear();
+    int indexOf(Object o);
+    void sort();
+    int lastIndexOf(Object o);
 
-    int size();
 
 }
